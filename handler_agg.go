ca7a15955b60d6a633fa4bec4b33e4657dc415e3
kairos4213 <jamesdkairos@gmail.com>
2024-11-29 15:08:00 -0600

    Add scrapeFeeds() and make agg loop

--- handler_agg.go
+++ handler_agg.go
@@ -2,18 +2,56 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"time"
+
+	"github.com/kairos4213/aligator/internal/database"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	if len(cmd.args) != 0 {
-		return fmt.Errorf("%v does not take any args", cmd.name)
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("%v takes one arg: <durationString>", cmd.name)
 	}
 
-	rssFeed, err := fetchRSSFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	timeBetweenRequests, err := time.ParseDuration(cmd.args[0])
 	if err != nil {
-		return fmt.Errorf("error fetching rss feed: %w", err)
+		return fmt.Errorf("error parsing time duration: %w", err)
 	}
-	fmt.Print(rssFeed)
+	fmt.Printf("Collecting feeds every %v\n\n", timeBetweenRequests)
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) error {
+	nextFeed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("error getting next feed to fetch: %w", err)
+	}
+
+	nextFeedParams := database.MarkFeedFetchedParams{
+		UpdatedAt:     time.Now().UTC(),
+		LastFetchedAt: sql.NullTime{Time: time.Now().UTC()},
+		ID:            nextFeed.ID,
+	}
+	err = s.db.MarkFeedFetched(context.Background(), nextFeedParams)
+	if err != nil {
+		return fmt.Errorf("error marking feed as fetched: %w", err)
+	}
+
+	feed, err := fetchRSSFeed(context.Background(), nextFeed.Url)
+	if err != nil {
+		return fmt.Errorf("error fetching feed: %w", err)
+	}
+
+	fmt.Println(feed.Channel.Title)
+	fmt.Println("=========================================================")
+	for _, item := range feed.Channel.Item {
+		fmt.Printf("Item Title: %s\n", item.Title)
+	}
+	fmt.Println("=========================================================")
 	return nil
 }
